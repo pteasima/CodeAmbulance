883fe5fee63dfcba79190f7c110156fed4d896d5
Petr Šíma <ptea.sima@gmail.com>
2021-10-14 15:02:10 +0200

    Initial Commit

---
+++ CodeAmbulance/CodeAmbulanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeAmbulanceApp.swift
+//  CodeAmbulance
+//
+//  Created by Petr Šíma on 14.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CodeAmbulanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
